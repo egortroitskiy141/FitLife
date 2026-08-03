397639eba401f4620ac83489b035756ee6375ced
Егор Троицкий <troitskiy_egor_06@mail.ru>
2026-08-03 19:23:54 +0300

    fix: замечания ревью — strip, try/except, константы

--- fit_life.py
+++ fit_life.py
@@ -1,28 +1,48 @@
 # Проект FitLife - MVP версия 1.0
 
+# Вместо «магических цифр»
+WATER_ML_PER_KG = 30
+ML_IN_LITER = 1000
+
 
 # 1. Знакомство
 # TODO: Спроси у пользователя имя и сохрани в переменную user_name
-user_name = input("Как вас зовут? ")
+# .strip() убирает пробелы до и после имени
+user_name = input("Как вас зовут? ").strip()
 # TODO: Спроси возраст и сохрани в переменную user_age (не забудь
 # преобразовать в число)
-user_age = int(input("Сколько вам лет? "))
+while True:
+    try:
+        user_age = int(input("Сколько вам лет? "))
+        break
+    except ValueError:
+        print("Ошибка: введите возраст целым числом, например 25.")
 
 # 2. Сбор данных
 # TODO: Запроси вес (в кг) и сохрани в user_weight (тип float)
-user_weight = float(input("Какой у вас вес в кг? "))
+while True:
+    try:
+        user_weight = float(input("Какой у вас вес в кг? "))
+        break
+    except ValueError:
+        print("Ошибка: введите вес числом, например 70.5.")
 # TODO: Запроси рост (в метрах, например 1.75) и сохрани в user_height
 # (тип float)
-user_height = float(input("Какой у вас рост в метрах? "))
+while True:
+    try:
+        user_height = float(input("Какой у вас рост в метрах? "))
+        break
+    except ValueError:
+        print("Ошибка: введите рост числом, например 1.75.")
 
 # 3. Логика расчетов (функции как "черный ящик": используем арифметику)
 # Формула ИМТ: вес разделить на (рост в квадрате)
-# TODO: Рассчитай bmi (Индекс массы тела)
+# TODO: Рассчитай bmi (индекс массы тела)
 bmi = round(user_weight / (user_height ** 2), 1)
 
 # Подсчет воды: вес * 30 мл
 # TODO: Рассчитай water_needed
-water_needed = user_weight * 30 / 1000
+water_needed = user_weight * WATER_ML_PER_KG / ML_IN_LITER
 
 # 4. Вывод красивого результата
 # TODO: Используй f-строку, чтобы вывести приветствие, например:
